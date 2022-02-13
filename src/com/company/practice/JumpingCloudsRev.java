1edf12d6bb77420e19043ea36dcb9cbe1334c5a6
Tfauves <tfauvelle@gmail.com>
2022-02-13 16:42:11 -0500

    clouds rev passes all but case 8

--- src/com/company/practice/JumpingCloudsRev.java
+++ src/com/company/practice/JumpingCloudsRev.java
@@ -8,27 +8,44 @@ package com.company.practice;
 public class JumpingCloudsRev {
     public static void main(String[] args) {
 //        1, 1, 1, 0, 1, 1, 0, 0, 0, 0
-        int[] clouds = new int[] {0, 0, 1, 0, 0, 1, 1, 0};
-//        int[] clouds = new int[] {1, 1, 1, 0, 1, 1, 0, 0, 0, 0};
+//        int[] clouds = new int[] {0, 0, 1, 0, 0, 1, 1, 0};
+        int[] clouds = new int[] {1, 1, 1, 0, 1, 1, 0, 0, 0, 0};
 
-        jumpingOnClouds(clouds, 2);
+        jumpingOnClouds(clouds, 3);
     }
 
     static int jumpingOnClouds(int[] clouds, int jumpDistance) {
         int energy = 100;
-        for (int i = 0; i < clouds.length; i = i + jumpDistance) {
-            int cloudType = clouds[i];
+        for (int i = 0; i < clouds.length; i += jumpDistance) {
+           int cloudType = clouds[i];
 
-            if (cloudType != 0) {
-                energy -= 2;
-            }
-            energy -= 1;
+
+           if (cloudType != 0) {
+               energy -= 2;
+           }
+           energy -= 1;
         }
 
         System.out.println(energy);
 
         return energy;
 
-
     }
+
+//    static int jumpingOnClouds(int[] clouds, int jumpDistance) {
+//        int energy = 100;
+//        for (int i = 0; i < clouds.length; i += jumpDistance) {
+//            int cloudType = clouds[i];
+//
+//            if (cloudType != 0) {
+//                energy -= 2;
+//            }
+//            energy -= 1;
+//        }
+//
+//        System.out.println(energy);
+//
+//        return energy;
+//
+//    }
 }
