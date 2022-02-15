3006bc885b2182a2210f7d63432c6face334d96e
Tfauves <tfauvelle@gmail.com>
2022-02-15 12:41:59 -0500

    sherlock anagram attempt

--- src/com/company/practice/SherlockAnagrams.java
+++ src/com/company/practice/SherlockAnagrams.java
@@ -1,4 +1,30 @@
 package com.company.practice;
 
+import java.util.*;
+
 public class SherlockAnagrams {
+    public static void main(String[] args) {
+        String testString = "ifailuhkqq";
+
+        sherlockAndAnagrams(testString);
+    }
+
+
+    public static int sherlockAndAnagrams(String s) {
+//        Map<Character, Integer> amountOfLetters =  new HashMap<>();
+        Stack<Character> letters = new Stack<>();
+        for (char letter : s.toCharArray()){
+            letters.push(letter);
+        }
+//        if (!amountOfLetters.containsKey(letter)) {
+//            amountOfLetters.put(letter, 1);
+//        }else {
+//            amountOfLetters.put(letter, amountOfLetters.get(letter) +1);
+//        }
+//
+//        System.out.println(amountOfLetters);
+        System.out.println(letters);
+
+        return 0;
+    }
 }
