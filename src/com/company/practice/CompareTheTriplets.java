657d31de1190ba2a23194b5274d6885134c2a65a
Tfauves <tfauvelle@gmail.com>
2022-02-15 12:44:23 -0500

    practice algo compare the triplets

---
+++ src/com/company/practice/CompareTheTriplets.java
@@ -0,0 +1,42 @@
+package com.company.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompareTheTriplets {
+    public static void main(String[] args) {
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        b.add(3);
+        b.add(2);
+        b.add(1);
+
+
+        System.out.println(compareTriplets(a,b));
+
+    }
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        Integer aScore = 0;
+        Integer bScore = 0;
+        List<Integer> scores = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) > b.get(i)) {
+                aScore++;
+            }
+
+            if (b.get(i) > a.get(i)) {
+                bScore++;
+            }
+        }
+
+        scores.add(aScore);
+        scores.add(bScore);
+
+        return scores;
+    }
+}
