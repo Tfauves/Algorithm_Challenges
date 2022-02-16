0ba48ceb594c0328c0ddc080f8e02d0fcb60311c
Tfauves <tfauvelle@gmail.com>
2022-02-16 16:59:13 -0500

    use of hash map re implemented for attempted solve

--- src/com/company/practice/SherlockAnagrams.java
+++ src/com/company/practice/SherlockAnagrams.java
@@ -4,26 +4,27 @@ import java.util.*;
 
 public class SherlockAnagrams {
     public static void main(String[] args) {
-        String testString = "ifailuhkqq";
+        String testString = "abba";
 
         sherlockAndAnagrams(testString);
     }
 
 
     public static int sherlockAndAnagrams(String s) {
-//        Map<Character, Integer> amountOfLetters =  new HashMap<>();
-        Stack<Character> letters = new Stack<>();
+        Map<Character, Integer> amountOfLetters =  new HashMap<>();
+
+
         for (char letter : s.toCharArray()){
-            letters.push(letter);
+        if (!amountOfLetters.containsKey(letter)) {
+            amountOfLetters.put(letter, 1);
+        }else {
+            amountOfLetters.put(letter, amountOfLetters.get(letter) +1);
         }
-//        if (!amountOfLetters.containsKey(letter)) {
-//            amountOfLetters.put(letter, 1);
-//        }else {
-//            amountOfLetters.put(letter, amountOfLetters.get(letter) +1);
-//        }
-//
-//        System.out.println(amountOfLetters);
-        System.out.println(letters);
+
+
+        }
+        System.out.println(amountOfLetters);
+
 
         return 0;
     }
