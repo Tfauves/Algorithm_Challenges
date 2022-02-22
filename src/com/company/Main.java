6278ff76ddcdcfea3fa7050ffa47f7628bc67333
Tfauves <tfauvelle@gmail.com>
2022-02-22 12:44:00 -0500

    linked list class with methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,13 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.removeLast();
+
+        System.out.println(list.indexOf(40));
 
     }
 
