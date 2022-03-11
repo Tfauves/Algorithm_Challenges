e95b847428b55b99ba2b79480490f2fd3adbb901
Tfauves <tfauvelle@gmail.com>
2022-03-11 16:06:46 -0500

    solved diagonalDifference challenge

--- src/com/company/hackerRank/prep/DiagonalDifference.java
+++ src/com/company/hackerRank/prep/DiagonalDifference.java
@@ -34,11 +34,34 @@ public class DiagonalDifference {
     }
 
 
-
     public static int diagonalDifference(List<List<Integer>> arr) {
+
+        // Final passing solution
+        int absDiff = 0;
+        int rightToLeft = 0;
+        int leftToRight =0;
+        int length = arr.size() -1;
+
+        for (int i = 0; i < arr.size(); i++) {
+            leftToRight += arr.get(i).get(i);
+            rightToLeft += arr.get(i).get(length --);
+
+        }
+
+        if (rightToLeft <= 0 && leftToRight <= 0) {
+            absDiff = leftToRight - rightToLeft;
+        } else {
+            absDiff = rightToLeft - leftToRight;
+        }
+
+
+        System.out.println(absDiff);
+        return Math.abs(absDiff);
 
 
     }
+
+}
 
 
 
@@ -74,4 +97,34 @@ public class DiagonalDifference {
 //
 //        return absDiff;
 
-}
+
+
+
+
+    /// attempt only passes 7 of 11
+//
+//    int absDiff = 0;
+//    int rightToLeft = 0;
+//    int leftToRight =0;
+//    int length = arr.size() -1;
+//
+//        for (int i = 0; i < arr.size(); i++) {
+//        leftToRight += arr.get(i).get(i);
+//        rightToLeft += arr.get(i).get(length --);
+//
+//        }
+//
+//        if (rightToLeft < 0 && leftToRight < 0) {
+//        absDiff = leftToRight - rightToLeft;
+//        } else {
+//        absDiff = rightToLeft - leftToRight;
+//        }
+//
+//
+//
+//        return absDiff;
+//
+//
+//        }
+
+
