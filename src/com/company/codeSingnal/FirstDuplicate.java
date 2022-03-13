1579a8eeade63467f6b4a40039210185d8125283
Tfauves <tfauvelle@gmail.com>
2022-03-13 16:29:09 -0400

    new challenge: codesignal firstDuplicate

--- src/com/company/codeSingnal/FirstDuplicate.java
+++ src/com/company/codeSingnal/FirstDuplicate.java
@@ -1,36 +1,63 @@
 package com.company.codeSingnal;
 
-// TODO: 2/26/2022 finish 
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO: 3/13/2022 attempt passes all but 2 time complexity cases.
 public class FirstDuplicate {
     public static void main(String[] args) {
-        int[] arr = new int[] {2, 1, 3, 5, 3, 2};
+//        int[] test = new int[] {1, 1, 2, 2, 1};
+        int[] test = new int[] {2, 1, 3, 5, 3, 2};
 
-        solution(arr);
+        solution(test);
+
     }
 
-    public static int solution(int[] a) {
-        int matches = 0;
+    static int solution(int[] a) {
+        Map<Integer, Integer> dups = new HashMap<>();
+
+
         for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length; j++)
-
-                if (a[j] == a[i]) {
-
-                    matches = j;
-
-
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    dups.put(j, a[i]);
                 }
-
-
+            }
         }
 
+        if (dups.isEmpty()){
+            return -1;
+        }
 
-
-
-
-        return -1;
+        int minValue = Collections.min(dups.keySet());
+        System.out.println(minValue);
+        return dups.get(minValue);
 
     }
-
-
-
 }
+//  attempt solves 10 of 12
+//    int solution(int[] a) {
+//        Map<Integer, Integer> dups = new HashMap<>();
+//
+//        for (int i = 0; i < a.length -1; i++) {
+//            if (a[i] == a[i +1]) {
+//                dups.put(i, a[i]);
+//            }
+//
+//        }
+//
+//        for (Integer index : dups.keySet()) {
+//            int minIndex = Collections.min(dups.keySet());
+//
+//            if (index <= minIndex) {
+//                return dups.get(index);
+//            }
+//        }
+//
+//
+//        return -1;
+//
+//
+//    }
