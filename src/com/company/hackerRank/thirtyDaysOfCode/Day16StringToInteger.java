7dfb1ba55d3ef6964989fba393662ad132d51d44
Tranquility Base <tfauvelle@gmail.com>
2022-03-20 15:27:46 -0400

    hackerrank 30 days: day 16 completed

---
+++ src/com/company/hackerRank/thirtyDaysOfCode/Day16StringToInteger.java
@@ -0,0 +1,22 @@
+package com.company.hackerRank.thirtyDaysOfCode;
+
+public class Day16StringToInteger {
+    public class Solution {
+//        public static void main(String[] args) throws IOException {
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//
+//            String S = bufferedReader.readLine();
+//
+//
+//            bufferedReader.close();
+//
+//            Integer stringInt;
+//
+//            try {
+//                stringInt = Integer.parseInt(S);
+//                System.out.println(stringInt);
+//            }catch(NumberFormatException e) {
+//                System.out.println("Bad String");
+//            }
+    }
+}
