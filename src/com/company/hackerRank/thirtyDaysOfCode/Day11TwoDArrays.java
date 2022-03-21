4060d74ff6958135a1c89b886eb22de422a0d67a
Tranquility Base <tfauvelle@gmail.com>
2022-03-21 17:10:26 -0400

    hackerrank 30days: day 11 completeed

---
+++ src/com/company/hackerRank/thirtyDaysOfCode/Day11TwoDArrays.java
@@ -0,0 +1,26 @@
+package com.company.hackerRank.thirtyDaysOfCode;
+
+public class Day11TwoDArrays {
+    public static void main(String[] args) {
+
+    }
+
+    // 0 will always be greater than the sum of negative numbers so can not set to 0;
+//    int maxSum = Integer.MIN_VALUE;
+//        for (int row = 0; row < arr.size() -2; row++) {
+//            for (int col = 0; col < arr.get(row).size() -2; col++) {
+//                int total = 0;
+//                total += arr.get(row).get(col) + arr.get(row).get(col + 1) + arr.get(row).get(col + 2);
+//                total += arr.get(row + 1).get(col + 1);
+//                total += arr.get(row + 2).get(col) + arr.get(row + 2).get(col + 1) + arr.get(row + 2).get(col + 2);
+//
+//                if ( total > maxSum) {
+//                maxSum = total;
+//                }
+//            }
+//
+//        }
+//        System.out.println(maxSum);
+
+
+}
