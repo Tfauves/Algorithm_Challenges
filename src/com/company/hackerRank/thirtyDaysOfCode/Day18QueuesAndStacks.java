e015c579863e07ddb4778b2a637117cd8d25dfa4
Tranquility Base <tfauvelle@gmail.com>
2022-03-27 12:18:04 -0400

    hackerrank day 18: stacks and queues completed

--- src/com/company/hackerRank/thirtyDaysOfCode/Day18QueuesAndStacks.java
+++ src/com/company/hackerRank/thirtyDaysOfCode/Day18QueuesAndStacks.java
@@ -1,14 +1,31 @@
 package com.company.hackerRank.thirtyDaysOfCode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Day18QueuesAndStacks {
 
-    public static void main(String[] args) {
+    public class Solution {
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new ArrayDeque<>();
 
+        // Write your code here.
+        void pushCharacter(char ch) {
+            stack.push(ch);
+        }
 
+        void enqueueCharacter(char ch) {
+            queue.add(ch);
+        }
 
-    }
+        char popCharacter() {
+            return stack.pop();
+        }
 
-    static void solution(String[] args) {
+        char dequeueCharacter() {
+            return queue.remove();
+        }
 
     }
 }
