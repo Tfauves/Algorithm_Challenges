7da66f85ec7c05b6b9d07876e6e1b77a0b0d5c2e
Tranquility Base <tfauvelle@gmail.com>
2022-03-30 15:27:44 -0400

    new challenges: hackerrank 30 days day 20 completed

---
+++ src/com/company/hackerRank/thirtyDaysOfCode/Day20Sorting.java
@@ -0,0 +1,31 @@
+package com.company.hackerRank.thirtyDaysOfCode;
+
+public class Day20Sorting {
+//    Integer[] input = new Integer[a.size()];
+//
+//    input = a.toArray(input);
+//    int swapCount = 0;
+//
+//
+//        for (int i = 0; i < input.length; i++) {
+//
+//        for (int j = 1; j < input.length; j++) {
+//            if (input[j] < input[j - 1]) {
+//                int temp = input[j];
+//                input[j] = input[j - 1];
+//                input[j - 1] = temp;
+//                swapCount++;
+//
+//            }
+//        }
+//
+//
+//    }
+//    int firstEle = input[0];
+//    int lastEle = input[input.length -1];
+//
+//
+//        System.out.println("Array is sorted in " + swapCount + " swaps.");
+//        System.out.println("First Element: " + firstEle);
+//        System.out.println("Last Element: " + lastEle);
+}
