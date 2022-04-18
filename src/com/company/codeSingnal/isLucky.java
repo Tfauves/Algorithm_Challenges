cd4682dfb9cd680b897bfdf4fa9361acdb718996
Tranquility Base <tfauvelle@gmail.com>
2022-04-18 19:30:04 -0400

    new code signal challenge completed

---
+++ src/com/company/codeSingnal/isLucky.java
@@ -0,0 +1,37 @@
+package com.company.codeSingnal;
+
+public class isLucky {
+    public static void main(String[] args) {
+        solution(11);
+    }
+
+    static boolean solution(int n) {
+        String temp = Integer.toString(n);
+        int[] numbArr = new int[temp.length()];
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < temp.length(); i++) {
+            numbArr[i] = temp.charAt(i) - '0';
+
+        }
+
+        for (int i = 0; i < numbArr.length; i++) {
+
+            if ((int)i < numbArr.length / 2) {
+                sum1 += numbArr[i];
+            } else {
+                sum2 += numbArr[i];
+            }
+        }
+        if (sum1 == sum2) {
+            System.out.println("true");
+            return true;
+        }
+
+        System.out.println(false);
+        return false;
+
+
+
+    }
+}
