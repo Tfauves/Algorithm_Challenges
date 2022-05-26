1e7aed69199fc777020cadbf53b227ac6a92f0cb
Tranquility Base <tfauvelle@gmail.com>
2022-05-26 17:35:22 -0400

    new challenge completed

---
+++ src/com/company/hackerRank/prep/StringIntroduction.java
@@ -0,0 +1,34 @@
+package com.company.hackerRank.prep;
+
+public class StringIntroduction {
+    public static void main(String[] args) {
+
+        Stringer("hello", "java");
+
+    }
+
+    public static void Stringer(String A, String B) {
+        int lengthSum = (A.length() + B.length());
+
+        System.out.println(lengthSum);
+        int bigger = A.compareTo(B);
+
+        if (bigger > 0) {
+            System.out.println("Yes");
+        }
+        if (bigger < 0) {
+            System.out.println("No");
+        }
+        if (bigger == 0) {
+            System.out.println("No");
+        }
+        String newA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String newB = B.substring(0, 1).toUpperCase() + B.substring(1);
+
+        System.out.print(newA + " " + newB);
+
+
+    }
+
+
+}
