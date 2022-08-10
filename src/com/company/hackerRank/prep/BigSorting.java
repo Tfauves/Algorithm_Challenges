17bee800ca3880dad529dacd560e189c0545a157
Tranquility Base <tfauvelle@gmail.com>
2022-08-09 20:11:10 -0400

    new challenge

---
+++ src/com/company/hackerRank/prep/BigSorting.java
@@ -0,0 +1,33 @@
+package com.company.hackerRank.prep;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BigSorting {
+    public static void main(String[] args) {
+        List<String> test = Arrays.asList("6", "31415926535897932384626433832795", "1", "3", "10", "3", "5");
+        bigSort(test);
+
+    }
+
+
+    public static List<String> bigSort(List<String> unsorted) {
+        List<Integer> intNumbers = new ArrayList<>();
+        List<String> strNumbers = new ArrayList<>();
+
+        for (String nums : unsorted) {
+            intNumbers.add(Integer.valueOf(nums));
+        }
+        Collections.sort(intNumbers);
+
+        for (Integer nums : intNumbers) {
+            strNumbers.add(Integer.toString(nums));
+        }
+
+        System.out.println(strNumbers);
+        return strNumbers;
+    }
+}
