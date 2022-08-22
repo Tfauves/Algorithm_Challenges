7b8af9456b17fe69bcbe20daf74c642cf5e05cf6
Tranquility Base <tfauvelle@gmail.com>
2022-08-22 15:36:22 -0400

    challenge completed

---
+++ src/com/company/hackerRank/SQL/RevisingTheSeclectQuery.java
@@ -0,0 +1,10 @@
+package com.company.hackerRank.SQL;
+
+public class RevisingTheSeclectQuery {
+//    select *
+//    from city
+//    where
+//            (countrycode = "usa"
+//                    and
+//                    population > 100000);
+}
