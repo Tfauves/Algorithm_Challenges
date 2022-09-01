2b4e83284ffe38572851f129c219d90a6a87984b
Travis J <tfauvelle@gmail.com>
2022-09-01 08:12:14 -0400

    completed

---
+++ src/com/company/hackerRank/javaprep/JavaAbstractClass.java
@@ -0,0 +1,15 @@
+package com.company.hackerRank.javaprep;
+
+//public class JavaAbstractClass {
+//    class MyBook extends Book{
+//
+//
+//        @Override
+//        public void setTitle(String s) {
+//            super.title = s;
+//        }
+//
+//
+//
+//    }
+}
