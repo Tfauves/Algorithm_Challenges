1d387222423de537a86fbdda4024a77dd4f9e955
Tranquility Base <tfauvelle@gmail.com>
2022-09-02 12:20:32 -0400

    completed

---
+++ src/com/company/hackerRank/thirtyDaysOfCode/Day24MoreLinkedLists.java
@@ -0,0 +1,23 @@
+package com.company.hackerRank.thirtyDaysOfCode;
+
+public class Day24MoreLinkedLists {
+//    public static Node removeDuplicates(Node head) {
+//        Node prev = head;
+//        Node current = head.next;
+//
+//        while (current != null) {
+//            if ( prev.data != current.data ) {
+//                prev.next = current;
+//                prev = current;
+//                current = current.next;
+//
+//            } else {
+//                current = current.next;
+//            }
+//
+//        }
+//        prev.next = null;
+//        return head;
+//
+//    }
+//}
