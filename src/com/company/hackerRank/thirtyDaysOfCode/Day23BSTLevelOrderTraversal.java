9f2ee9985ee385d9cd345545cc0cee0e15303661
Tranquility Base <tfauvelle@gmail.com>
2022-09-02 12:19:16 -0400

    completed

---
+++ src/com/company/hackerRank/thirtyDaysOfCode/Day23BSTLevelOrderTraversal.java
@@ -0,0 +1,28 @@
+package com.company.hackerRank.thirtyDaysOfCode;
+
+import com.company.codeSingnal.LinkedList;
+import org.w3c.dom.Node;
+
+import java.util.Queue;
+
+public class Day23BSTLevelOrderTraversal {
+//    static void levelOrder(Node root){
+//        //Write your code here
+//        Queue<Node> btsQueue = new LinkedList<>();
+//        btsQueue.add(root);
+//        while(!btsQueue.isEmpty()) {
+//            Node myNode = btsQueue.remove();
+//            System.out.print(myNode.data + " ");
+//
+//            if(myNode.left != null) {
+//                btsQueue.add(myNode.left);
+//            }
+//
+//            if(myNode.right != null) {
+//                btsQueue.add(myNode.right);
+//            }
+//        }
+//
+//
+//    }
+}
