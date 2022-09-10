d4afe826a57e72fd46894507b9439e597762c8c4
Travis J <tfauvelle@gmail.com>
2022-09-10 16:56:12 -0400

    completed

--- src/com/company/hackerRank/threemonthprep/MarsExploration.java
+++ src/com/company/hackerRank/threemonthprep/MarsExploration.java
@@ -5,11 +5,12 @@ import java.util.ArrayDeque;
 public class MarsExploration {
 
     public static void main(String[] args) {
-        marsExploration("SOSSOSSOS");
+        marsExploration("SOSSPSSQSSOR");
         // SOSSPSSQSSOR
         // SOSSOT
         // SOSSOSSOS
     }
+
     public static int marsExploration(String s) {
         int mutLetters = 0;
         ArrayDeque<Character> message = new ArrayDeque<>();
@@ -20,29 +21,68 @@ public class MarsExploration {
 
         while (!message.isEmpty()) {
             char[] signal = {'S', 'O', 'S'};
-
-            for (Character sigLetter : signal) {
-                System.out.println(message.peek());
-                if (message.peek() == 'S') {
-                    message.pop();
+            for (int i = 0; i < signal.length; i++) {
+                Character pop = message.pop();
+                if (pop != signal[i]) {
+                    mutLetters++;
                 }
-                System.out.println(message.peek());
-                if (message.peek() == 'O') {
-                    message.pop();
-                }
-                System.out.println(message.peek());
-                if (message.peek() == 'S') {
-                    message.pop();
-                } else{
-                    break;
-                }
+//                System.out.println(pop + "signal letter " + signal[i]);
             }
-            if (message.isEmpty()) break;
-            mutLetters++;
-            message.pop();
+
 
         }
         System.out.println(mutLetters);
         return mutLetters;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //// attempt 1
+//    public static int marsExploration(String s) {
+//        int mutLetters = 0;
+//        ArrayDeque<Character> message = new ArrayDeque<>();
+//        for (Character letter : s.toCharArray()) {
+//            message.add(letter);
+//
+//        }
+//
+//        while (!message.isEmpty()) {
+//            char[] signal = {'S', 'O', 'S'};
+//
+//            for (Character sigLetter : signal) {
+//                System.out.println(message.peek());
+//                if (message.peek() == 'S') {
+//                    message.pop();
+//                }
+//                System.out.println(message.peek());
+//                if (message.peek() == 'O') {
+//                    message.pop();
+//                }
+//                System.out.println(message.peek());
+//                if (message.peek() == 'S') {
+//                    message.pop();
+//                } else{
+//                    break;
+//                }
+//            }
+//            if (message.isEmpty()) break;
+//            mutLetters++;
+//            message.pop();
+//
+//        }
+//        System.out.println(mutLetters);
+//        return mutLetters;
+//    }
+
