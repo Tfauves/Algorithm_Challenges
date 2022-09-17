1ca50f6c75884ff2e8657802bd576ef5f40c02a1
Travis J <tfauvelle@gmail.com>
2022-09-17 10:15:43 -0400

    comppleted

---
+++ out/production/AlgosAndPractice/com/company/hackerRank/SQL/basicselect/WeatherObservation10.sql
@@ -0,0 +1,11 @@
+--Query the list of CITY names
+--from STATION that do
+--not end with vowels.
+--Your result cannot contain duplicates.
+
+
+SELECT
+DISTINCT CITY
+FROM STATION
+WHERE Right(CITY,1)
+NOT in ('a', 'e', 'i', 'o', 'u');
