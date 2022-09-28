94c27e235dfee9af6b38355c101ffbc1e7d6cfd7
Travis J <tfauvelle@gmail.com>
2022-09-28 08:23:28 -0400

    completed

--- src/com/company/hackerRank/threemonthprep/CountingSort1.java
+++ src/com/company/hackerRank/threemonthprep/CountingSort1.java
@@ -18,34 +18,45 @@ public class CountingSort1 {
         // create a counting list that will hold each index of the input arr
         // iterate input arr if the index holds a value increment the value of the same index in the counting list
         // iterate the count list print the value of each non zero valued index
-
-        List<Integer> freqList = new ArrayList<>(arr.size());
-        for (int i = 0; i < arr.size() -1; i++) {
+        List<Integer> freqList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
             freqList.add(0);
-
-        }
-//        System.out.println(freqList);
-        for (int i = 0; i < arr.size(); i++) {
-            int checkValue = arr.get(i);
-//            System.out.println(checkValue);
-            for (int j = 0; j < freqList.size(); j++) {
-//                System.out.println(j);
-                if (checkValue == j) {
-                   Integer updateElementVal = freqList.get(j);
-                   updateElementVal++;
-//                    System.out.println(updateElementVal);
-                    freqList.set(j, updateElementVal);
-//                    System.out.println(freqList);
-                    break;
-                }
-
-            }
         }
 
-//        System.out.println(freqList);
+        for (Integer i : arr) {
+            freqList.set(i, freqList.get(i) + 1);
+        }
+        System.out.println(freqList);
         return freqList;
 
-
     }
-
 }
+
+//        List<Integer> freqList = new ArrayList<>(arr.size());
+//        for (int i = 0; i < arr.size() -1; i++) {
+//            freqList.add(0);
+//
+//        }
+////        System.out.println(freqList);
+//        for (int i = 0; i < arr.size(); i++) {
+//            int checkValue = arr.get(i);
+////            System.out.println(checkValue);
+//            for (int j = 0; j < freqList.size(); j++) {
+////                System.out.println(j);
+//                if (checkValue == j) {
+//                   Integer updateElementVal = freqList.get(j);
+//                   updateElementVal++;
+////                    System.out.println(updateElementVal);
+//                    freqList.set(j, updateElementVal);
+////                    System.out.println(freqList);
+//                    break;
+//                }
+//
+//            }
+//        }
+//
+////        System.out.println(freqList);
+//        return freqList;
+//
+//
+//    }
