bdd0fffd2df378540024ec3cd0b7abf8ba98525f
Tranquility Base <tfauvelle@gmail.com>
2022-10-13 19:07:37 -0400

    attempt

--- src/com/company/hackerRank/problemsolving/FlippingTheMatrix.java
+++ src/com/company/hackerRank/problemsolving/FlippingTheMatrix.java
@@ -1,18 +1,45 @@
 package com.company.hackerRank.problemsolving;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FlippingTheMatrix {
 
     public static void main(String[] args) {
+        List<List<Integer>> arr = new ArrayList<>();
+        arr.add(Arrays.asList(112,42,83,119));
+        arr.add(Arrays.asList(56,125,56,49));
+        arr.add(Arrays.asList( 15,78, 101, 43));
+        arr.add(Arrays.asList( 62,98,114,108));
+
+
+        flippingMatrix(arr);
 
     }
 
-    public static void reverseRow(List<Integer> flipRow) {
+    public static int flippingMatrix(List<List<Integer>> matrix) {
 
-        for (int i = 0, j = flipRow.size() -1; i < j; i++) {
-
+        int len = matrix.size();
+        int total = 0;
+        for(int i = 0; i < len/2; i++) {
+            for(int j = 0; j < len/2; j++) {
+                int count = matrix.get(i).get(j);
+                System.out.println(matrix.get(i).get(j));
+                count = Integer.max(count, matrix.get(i).get(len - 1 - j));
+                System.out.println(matrix.get(i).get(len - 1 - j));
+                count = Integer.max(count, matrix.get(len - 1 - i).get(j));
+                System.out.println(matrix.get(len - 1 - i).get(j));
+                count = Integer.max(count, matrix.get(len - 1 - i).get(len - 1 - j));
+                System.out.println( matrix.get(len - 1 - i).get(len - 1 - j));
+                total += count;
+//                System.out.println(count);
+            }
         }
+        System.out.println(total);
+        return total;
 
     }
+
+
 }
--- src/com/company/hackerRank/threemonthprep/MaximumPerimeterTriangle.java
+++ src/com/company/hackerRank/threemonthprep/MaximumPerimeterTriangle.java
@@ -7,25 +7,47 @@ import java.util.List;
 
 // TODO: 10/4/2022 still dont understand
 public class MaximumPerimeterTriangle {
-
+// Non-degenerate triangle − it is a triangle that has a positive area.
+// The condition for a non-degenerate triangle with sides a, b, c is
+// a + b > c   a + c > b   b + c > a
     public static void main(String[] args) {
         List<Integer> test = Arrays.asList(1, 1, 1, 3, 3);
+        // 113 expected output
         maximumPerimeterTriangle(test);
 
     }
 
     public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
         List<Integer> lengths = new ArrayList<>();
-        Collections.sort(sticks);
+////        Collections.sort(sticks);
+//
+//        for (int i = 0; i < sticks.size(); i++) {
+//
+//            for (int j = 0; j < sticks.size(); j++) {
+//                System.out.println(sticks.get(i) + " " + sticks.get(j));
+//            }
+//
+//        }
+//
+//
+//        return lengths;
 
-        for (int i = 0; i <= sticks.size() -1; i++) {
-
-            System.out.print(sticks.get(i));
-
+        List<List<Integer>> nums = new ArrayList<>();
+        int counter = 0;
+        for (int i = 0; i < sticks.size() - 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                counter++;
+                nums.add(Arrays.asList(sticks.get(j), sticks.get(j + 1), sticks.get(j + 2)));
+            }
+            if (i == 1){
+                break;
+            }
+            System.out.println(nums);
         }
+        // System.out.println(nums);
         return lengths;
-
     }
+
 
 
 }
