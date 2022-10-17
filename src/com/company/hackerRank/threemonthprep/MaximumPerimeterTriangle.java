363d0fdc156c36fb0fe31d51fef36e5f2f98728f
Travis J <tfauvelle@gmail.com>
2022-10-17 09:14:00 -0400

    attempt

--- src/com/company/hackerRank/threemonthprep/MaximumPerimeterTriangle.java
+++ src/com/company/hackerRank/threemonthprep/MaximumPerimeterTriangle.java
@@ -2,10 +2,9 @@ package com.company.hackerRank.threemonthprep;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-// TODO: 10/4/2022 still dont understand
+// TODO: 10/17/2022 cant get this  
 public class MaximumPerimeterTriangle {
 // Non-degenerate triangle − it is a triangle that has a positive area.
 // The condition for a non-degenerate triangle with sides a, b, c is
@@ -18,8 +17,40 @@ public class MaximumPerimeterTriangle {
     }
 
     public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
-        List<Integer> lengths = new ArrayList<>();
-////        Collections.sort(sticks);
+        List<List<Integer>> lengths = new ArrayList<>();
+        List<Integer> validTris = new ArrayList<>();
+        for (int i = 0; i < sticks.size() -2; i++) {
+           lengths.add(sticks.subList(i , i + 3));
+        }
+        System.out.println(lengths);
+
+        isVaildTri(lengths.get(1));
+        return validTris;
+    }
+
+    public static boolean isVaildTri(List<Integer> input) {
+        boolean isValid = false;
+        int sum = 0;
+        for (int i = 0; i < input.size() -1; i++) {
+            sum = input.get(i) + input.get(i + 1);
+            if (sum > input.get(i)){
+                isValid = true;
+            }
+            System.out.println(sum);
+
+        }
+        System.out.println(isValid);
+        return isValid;
+    }
+
+
+}
+
+
+////////////// first attempt ////////////////////////
+//    public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
+//        List<Integer> lengths = new ArrayList<>();
+//        Collections.sort(sticks);
 //
 //        for (int i = 0; i < sticks.size(); i++) {
 //
@@ -29,25 +60,27 @@ public class MaximumPerimeterTriangle {
 //
 //        }
 //
-//
 //        return lengths;
-
-        List<List<Integer>> nums = new ArrayList<>();
-        int counter = 0;
-        for (int i = 0; i < sticks.size() - 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                counter++;
-                nums.add(Arrays.asList(sticks.get(j), sticks.get(j + 1), sticks.get(j + 2)));
-            }
-            if (i == 1){
-                break;
-            }
-            System.out.println(nums);
-        }
-        // System.out.println(nums);
-        return lengths;
-    }
+//
+//    }
 
 
-
-}
+//////////// second attempt //////////////////////
+//    public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
+//
+//        List<List<Integer>> nums = new ArrayList<>();
+//        int counter = 0;
+//        for (int i = 0; i < sticks.size() - 2; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                counter++;
+//                nums.add(Arrays.asList(sticks.get(j), sticks.get(j + 1), sticks.get(j + 2)));
+//            }
+//            if (i == 1){
+//                break;
+//            }
+//            System.out.println(nums);
+//        }
+//        // System.out.println(nums);
+//        return lengths;
+//    }
+//
