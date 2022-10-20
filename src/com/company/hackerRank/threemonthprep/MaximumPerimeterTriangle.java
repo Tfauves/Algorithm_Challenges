d292561e0de093b6cbf366fa499db49e5e1a22e6
Tranquility Base <tfauvelle@gmail.com>
2022-10-20 10:49:11 -0400

    attempt

--- src/com/company/hackerRank/threemonthprep/MaximumPerimeterTriangle.java
+++ src/com/company/hackerRank/threemonthprep/MaximumPerimeterTriangle.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// TODO: 10/19/2022 getting closer
+// TODO: 10/19/2022 only passing 5 test cases
 public class MaximumPerimeterTriangle {
     // Non-degenerate triangle − it is a triangle that has a positive area.
-// The condition for a non-degenerate triangle with sides a, b, c is
-// a + b > c   a + c > b   b + c > a
+    // The condition for a non-degenerate triangle with sides a, b, c is
+    // a + b > c   a + c > b   b + c > a
     public static void main(String[] args) {
-        List<Integer> test = Arrays.asList(1, 1, 1, 3, 3);
+        List<Integer> test = Arrays.asList(3, 9, 2, 15, 3);
         // 113 expected output
         maximumPerimeterTriangle(test);
 
@@ -19,29 +19,44 @@ public class MaximumPerimeterTriangle {
     public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
         List<List<Integer>> lengths = new ArrayList<>();
         List<Integer> validTris = new ArrayList<>();
+        int maxSum = 0;
+        int totalSum = 0;
         for (int i = 0; i < sticks.size() - 2; i++) {
             lengths.add(sticks.subList(i, i + 3));
         }
+
 //        System.out.println(lengths);
         for (List<Integer> length : lengths) {
-            int maxSum = 0;
+
             int a = length.get(0);
             int b = length.get(1);
             int c = length.get(2);
             int sum1 = a + b;
             int sum2 = a + c;
             int sum3 = b + c;
+            totalSum = a + b + c;
 
 
             if(sum1 > c && sum2 > b && sum3 > a) {
+                if (validTris.isEmpty()) {
+                    validTris.addAll(length);
+                }
 
-                validTris.addAll(length);
+                if(totalSum > maxSum) {
+                    validTris.clear();
+                    maxSum = totalSum;
+                    validTris.addAll(length);
+
+                }
+
+
             }
 //            System.out.println(a);
 //            System.out.println(b);
 //            System.out.println(c);
 
         }
+        if (validTris.isEmpty()) validTris.add(-1);
 
 
         System.out.println(validTris);
