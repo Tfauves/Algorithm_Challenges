6793b516ba2cc481b3ef849c86446fecfe7ef98a
Travis J <tfauvelle@gmail.com>
2022-10-23 13:07:56 -0400

    completed

---
+++ src/com/company/hackerRank/javaprep/JavaFactoryPattern.java
@@ -0,0 +1,19 @@
+package com.company.hackerRank.javaprep;
+
+public class JavaFactoryPattern {
+    public static void main(String[] args) {
+//        if(order.equals("pizza")){
+//            // System.out.println("The factory returned class pizza");
+//            Pizza obj=new Pizza();
+//            return obj;
+//        }
+//        else{
+//            //System.out.println("The factory returned class cake");
+//            Cake obj=new Cake();
+//            return obj;
+//        }
+    }
+
+
+
+}
