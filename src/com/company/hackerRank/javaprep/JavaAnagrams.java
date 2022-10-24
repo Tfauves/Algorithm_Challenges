7f52a64efc87fe51e65039fde2c4c6332e32d694
Travis J <tfauvelle@gmail.com>
2022-10-24 10:35:19 -0400

    attempt

--- src/com/company/hackerRank/javaprep/JavaAnagrams.java
+++ src/com/company/hackerRank/javaprep/JavaAnagrams.java
@@ -1,37 +1,63 @@
 package com.company.hackerRank.javaprep;
 
-import java.util.Locale;
+import java.util.Arrays;
+import java.util.Scanner;
 
+// TODO: 10/24/2022 still not passing all cases 
 public class JavaAnagrams {
-    // TODO: 9/22/2022 not passing last case 
-    // https://www.tutorialspoint.com/java/java_string_replacefirst.htm
-    // https://www.w3schools.com/java/ref_string_indexof.asp
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        isAnagram("anagramm", "marganaa");
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+//        "anagramm"
+//        "marganaa"
+        isAnagram(a, b);
 
     }
 
     public static boolean isAnagram(String a, String b) {
 
-        for (int i = b.length() -1; i >= 0; i--) {
-            char bLetters = b.toLowerCase().charAt(i);
-            a = a.toLowerCase();
+        if(a.length() != b.length()) return false;
 
-//            System.out.println(bLetters);
-//            System.out.println(a);
+        a = a.toLowerCase();
+        b = b.toLowerCase();
 
-            //An int value, representing the index of the first occurrence of the character in the string,
-            // or -1 if it never occurs
-            if (a.indexOf(bLetters) == -1) {
-                System.out.println("false");
-                return false;
-            }
+        char[] charArrA = a.toCharArray();
+        char[] charArrB = b.toCharArray();
+
+        Arrays.sort(charArrA);
+        Arrays.sort(charArrB);
+
+        boolean result = Arrays.equals(charArrA, charArrB);
+
+        if (!result) {
+            System.out.println("Not Anagrams");
+            return false;
         }
 
-        System.out.println("true");
+        System.out.println("Anagrams");
         return true;
-
     }
 
 }
+
+
+///// first attempt/////////
+//        for (int i = b.length() -1; i >= 0; i--) {
+//            char bLetters = b.toLowerCase().charAt(i);
+//            a = a.toLowerCase();
+//
+////            System.out.println(bLetters);
+////            System.out.println(a);
+//
+//            //An int value, representing the index of the first occurrence of the character in the string,
+//            // or -1 if it never occurs
+//            if (a.indexOf(bLetters) == -1) {
+//                System.out.println("false");
+//                return false;
+//            }
+//        }
+//
+//        System.out.println("true");
+//        return true;
