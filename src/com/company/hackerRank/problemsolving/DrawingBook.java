c3aad140474f2dd1237f6a640b90238c796b128a
Travis J <tfauvelle@gmail.com>
2022-10-29 21:16:54 -0400

    better attempt

--- src/com/company/hackerRank/problemsolving/DrawingBook.java
+++ src/com/company/hackerRank/problemsolving/DrawingBook.java
@@ -1,6 +1,6 @@
 package com.company.hackerRank.problemsolving;
 
-// TODO: 10/29/2022 passing 6 cases so far 
+// TODO: 10/29/2022 passing 16 cases so far
 public class DrawingBook {
     public static void main(String[] args) {
         pageCount(5, 4);
@@ -12,37 +12,50 @@ public class DrawingBook {
 
         // page turn on ood len at every odd element
 
-        int pagesToTurn = 0;
+        int pagesToTurnFront = 0;
+        int pagesToTurnBack = 0;
 
         if((bookLen / 2) < targetPage) {
             for (int i = bookLen; i >= targetPage; i--) {
                 if (bookLen % 2 == 0) {
-                    pagesToTurn++;
+                    pagesToTurnBack++;
                 }
                 if (i == targetPage) {
                     break;
                 }
-                pagesToTurn++;
+                pagesToTurnBack++;
             }
         } else {
             for (int i = 1; i <= bookLen; i++) {
-
+                if (i == targetPage) {
+                    break;
+                }
                 if (i % 2 == 1) {
                     if (i == targetPage) {
                         break;
                     }
-                    pagesToTurn++;
+                    pagesToTurnFront++;
 
                 }
 
             }
+        }
 
-
+        if (pagesToTurnBack == 0) {
+            return pagesToTurnFront;
+        } else if (pagesToTurnFront == 0) {
+            return pagesToTurnBack;
+        } else {
+            System.out.println(Math.min(pagesToTurnFront, pagesToTurnBack));
+            return Math.min(pagesToTurnFront, pagesToTurnBack);
         }
 
 
-        System.out.println(pagesToTurn);
-        return pagesToTurn;
+
+
+//
+//        System.out.println(pagesToTurn);
+//        return pagesToTurn;
 
     }
 
