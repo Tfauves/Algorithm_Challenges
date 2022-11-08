fc1eb7d9b3892ac33d771a49dbbfd0d4e473b76e
Travis J <tfauvelle@gmail.com>
2022-11-08 15:58:57 -0500

    attempt with hashmap

--- src/com/company/hackerRank/problemsolving/Anagram.java
+++ src/com/company/hackerRank/problemsolving/Anagram.java
@@ -1,13 +1,15 @@
 package com.company.hackerRank.problemsolving;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Anagram {
 
     public static void main(String[] args) {
 
-        anagram("aaabbb");
+        anagram("xaxbbbxx");
 //        aaabbb ab abc mnop xyyx xaxbbbxx
 //        3 1-1 2 0 1
 
@@ -28,19 +30,38 @@ public class Anagram {
         String sub2 = s.substring(middle);
         System.out.println(sub1);
         System.out.println(sub2);
-        List<Character> chList1 = new ArrayList<>();
+        Map<Character, Integer> charFreq = new HashMap<>();
 
-        for (Character letter : sub1.toCharArray()) {
-            chList1.add(letter);
+        for(Character letter : sub1.toCharArray()) {
+            if (!charFreq.containsKey(letter)) {
+                charFreq.put(letter, 1);
+            } else {
+                charFreq.put(letter, charFreq.get(letter) +1);
+            }
         }
-        System.out.println(chList1);
 
-        for (int i = 0; i <= chList1.size() -1; i++) {
-            if (!chList1.contains(sub2.charAt(i))) {
-                minNumberChange++;
+            for (int i = 0; i < sub2.length(); i++) {
+                if (charFreq.containsKey(sub2.charAt(i))) {
+                    charFreq.put(sub2.charAt(i), charFreq.get(sub2.charAt(i)) -1);
+
+                }
+
             }
 
-        }
+
+        System.out.println(charFreq);
+
+//        for (Character letter : sub1.toCharArray()) {
+//            chList1.add(letter);
+//        }
+//        System.out.println(chList1);
+//
+//        for (int i = 0; i <= chList1.size() -1; i++) {
+//            if (!chList1.contains(sub2.charAt(i))) {
+//                minNumberChange++;
+//            }
+//
+//        }
         System.out.println(minNumberChange);
 //        System.out.println(chList1.size());
 
