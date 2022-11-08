ecc7076ef2f9116e183e34980037dab7d954ba0d
Travis J <tfauvelle@gmail.com>
2022-11-07 20:08:10 -0500

    remote work

--- src/com/company/hackerRank/problemsolving/Anagram.java
+++ src/com/company/hackerRank/problemsolving/Anagram.java
@@ -7,7 +7,7 @@ public class Anagram {
 
     public static void main(String[] args) {
 
-        anagram("xaxbbbxx");
+        anagram("aaabbb");
 //        aaabbb ab abc mnop xyyx xaxbbbxx
 //        3 1-1 2 0 1
 
@@ -34,30 +34,18 @@ public class Anagram {
             chList1.add(letter);
         }
         System.out.println(chList1);
-//        for (int i = 0; i < sub1.length(); i++) {
-//            System.out.println(sub1.charAt(i));
-//            if (sub2.indexOf(sub1.charAt(i)) == -1) {
-//                System.out.println("no");
-//                minNumberChange++;
-//            }
-//
-//        }
 
-        for (int i = 0; i < chList1.size() -1; i++) {
-
-            for (int j = 0; j < sub2.length(); j++) {
-                if (chList1.get(i) == sub2.charAt(j)) {
-                    System.out.println("match");
-                    chList1.remove(chList1.get(i));
-                    minNumberChange++;
-                }
+        for (int i = 0; i <= chList1.size() -1; i++) {
+            if (!chList1.contains(sub2.charAt(i))) {
+                minNumberChange++;
             }
 
         }
-//        System.out.println(minNumberChange);
+        System.out.println(minNumberChange);
 //        System.out.println(chList1.size());
-        System.out.println(Math.abs(minNumberChange - sub2.length()));
-        return Math.abs(minNumberChange - sub2.length());
+
+
+        return minNumberChange;
     }
 
 }
