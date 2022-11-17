b5a0d61f676eefdab7b107e342559fb3deb43ac1
Travis J <tfauvelle@gmail.com>
2022-11-17 10:01:27 -0500

    completed

--- src/com/company/hackerRank/problemsolving/MinimumDistances.java
+++ src/com/company/hackerRank/problemsolving/MinimumDistances.java
@@ -1,6 +1,8 @@
 package com.company.hackerRank.problemsolving;
 // TODO: 10/6/2022 not passing all cases 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MinimumDistances {
@@ -10,41 +12,51 @@ public class MinimumDistances {
     }
 
     public static int minimumDistances(List<Integer> a) {
-        /*
-        locate elements that are equal
-        if no elements are equal return -1;
-        else
-        calculate the number of indices between them
-        distance = j - i
-        return the min distance
-        1). iterate the list
-        2). compare each element to the next for equality
-        3). if none are equal return -1
-        4). else need a counter to count indices between i and j
-        5). compare each count to current count only keep the min count
-        6). return count
-        */
+        int minDist = -1;
+        List<Integer> distances = new ArrayList<>();
 
-        int distance = 10000;
-
-        for (int current = 0; current < a.size() -1; current++) {
-
-            for (int next = current + 1; next < a.size(); next++) {
-                if ( a.get(current) == a.get(next)) {
-                    System.out.println(current);
-                    System.out.println(next);
-                    int diff = Math.abs(current - next);
-                    if(diff < distance) {
-                        distance = diff;
-                    }
+        for (int i = 0; i < a.size() -1; i++) {
+            for (int j = i +1; j < a.size(); j++) {
+                int diff = 0;
+                if (a.get(i).equals(a.get(j))) {
+                    diff = j - i;
+                   distances.add(diff);
                 }
-                // System.out.println(a.get(current));
-                // System.out.println(a.get(next));
+
             }
         }
 
+        if (distances.isEmpty()) {
+            return minDist;
+        } else {
+            Collections.sort(distances);
+        }
 
-//        System.out.println(distance);
-        return distance;
+        System.out.println(distances.get(0));
+        return distances.get(0);
+
     }
 }
+
+///first attempt////////
+//  int distance = 10000;
+//
+//        for (int current = 0; current < a.size() -1; current++) {
+//
+//            for (int next = current + 1; next < a.size(); next++) {
+//                if ( a.get(current) == a.get(next)) {
+//                    System.out.println(current);
+//                    System.out.println(next);
+//                    int diff = Math.abs(current - next);
+//                    if(diff < distance) {
+//                        distance = diff;
+//                    }
+//                }
+//                // System.out.println(a.get(current));
+//                // System.out.println(a.get(next));
+//            }
+//        }
+//
+//
+////        System.out.println(distance);
+//        return distance;
