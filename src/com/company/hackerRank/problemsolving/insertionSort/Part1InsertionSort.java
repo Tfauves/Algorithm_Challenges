8887cf56a59f9976f60f9a3355035aa49fa54df7
Travis J <tfauvelle@gmail.com>
2022-11-20 11:40:36 -0500

    complete

--- src/com/company/hackerRank/problemsolving/insertionSort/Part1InsertionSort.java
+++ src/com/company/hackerRank/problemsolving/insertionSort/Part1InsertionSort.java
@@ -3,15 +3,16 @@ package com.company.hackerRank.problemsolving.insertionSort;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO: 11/20/2022 works right but dosent pass becaue of print format probs 
 public class Part1InsertionSort {
     public static void main(String[] args) {
         List<Integer> test = new ArrayList<>();
 //        5
 //        2 4 6 8 3
+        test.add(1);
         test.add(2);
         test.add(4);
-        test.add(6);
-        test.add(8);
+        test.add(5);
         test.add(3);
 
         insertionSort1(5, test);
@@ -23,15 +24,28 @@ public class Part1InsertionSort {
         // When the next lower indexed value is smaller than , insert the stored value at the current index and print the entire array.
         // Each time your test fails, copy the value at the lower index to the current index and print your array
         // When the next lower indexed value is smaller than , insert the stored value at the current index and print the entire array.
+        int temp = arr.get(arr.size() -1);
+//        System.out.println(temp);
+        for (int i = arr.size() -1; i >= 0; i--) {
+            int next = i -1;
 
+            if (arr.get(next) > arr.get(i)) {
+              arr.set(i, arr.get(next));
 
-        for (int i = arr.size() -2; i >= 0; i--) {
-            int insertValue = arr.get(arr.size() -1);
-            int current = arr.get(i);
-            int next = arr.get(i - 1);
-            System.out.println("insert " + insertValue + " current " + current + " next " + next);
+            }
+            if (arr.get(i) > arr.get(next) && arr.get(next) > temp) {
+                arr.set(i, arr.get(next));
+            }
 
+            if (arr.get(next) < temp ) {
+                arr.set(i, temp);
+                System.out.println(arr);
+                break;
+            }
+
+            System.out.println(arr);
         }
+
 
     }
 
