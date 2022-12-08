b12308b9bee9adb3e388c3ef9913b265859cd893
Travis J <tfauvelle@gmail.com>
2022-12-08 09:11:58 -0500

    completed

---
+++ src/com/company/codeSingnal/AddBorder.java
@@ -0,0 +1,10 @@
+package com.company.codeSingnal;
+
+public class AddBorder {
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
--- src/com/company/hackerRank/javaprep/JavaStringTokens.java
+++ src/com/company/hackerRank/javaprep/JavaStringTokens.java
@@ -1,28 +1,47 @@
 package com.company.hackerRank.javaprep;
-
-
 import java.util.Scanner;
 
-// TODO: 9/13/2022 will not pass testcase 9 
 public class JavaStringTokens {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
+        if (!scan.hasNext()){
+            System.out.println("0");
+        } else {
 
-        String[] strSplit = s.trim().split("['!?,._@ ]+");
-      if (s.equals("")) {
+            String s = scan.nextLine();
 
-          System.out.println(0);
-          return;
-      }
-        System.out.println(strSplit.length);
+            String[] strSplit = s.trim().split("['!?,._@ ]+");
 
-        for(int i = 0; i < strSplit.length; i++) {
-            System.out.println(strSplit[i]);
+            System.out.println(strSplit.length);
+
+            for(int i = 0; i < strSplit.length; i++) {
+                System.out.println(strSplit[i]);
+            }
+
+            scan.close();
         }
-
-        scan.close();
     }
+
 }
 // He is a very very good boy, isn't he?
+
+/// first attempt passed all but test case 9 in jackerrank but worked here
+//    public static void main(String[] args) {
+//        Scanner scan = new Scanner(System.in);
+//        String s = scan.nextLine();
+//
+//        String[] strSplit = s.trim().split("['!?,._@ ]+");
+//        if (s.equals("")) {
+//
+//            System.out.println(0);
+//            return;
+//        }
+//        System.out.println(strSplit.length);
+//
+//        for(int i = 0; i < strSplit.length; i++) {
+//            System.out.println(strSplit[i]);
+//        }
+//
+//        scan.close();
+//    }
