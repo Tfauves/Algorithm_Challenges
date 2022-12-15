3794055ae71ea192bcfaeebe6f59f6c7861201de
Travis J <tfauvelle@gmail.com>
2022-12-15 09:16:45 -0500

    completed

---
+++ src/com/company/hackerRank/javaprep/JavaInterface.java
@@ -0,0 +1,21 @@
+package com.company.hackerRank.javaprep;
+
+public class JavaInterface {
+    public static void main(String[] args) {
+
+    }
+//
+//    class MyCalculator implements AdvancedArithmetic {
+//        public int divisor_sum(int n) {
+//            int dSum = 0;
+//            for (int i = 1; i <= n; i++) {
+//                if ( n % i == 0) {
+//                    dSum += i;
+//                }
+//            }
+//
+//            return dSum;
+//        }
+//    }
+
+}
