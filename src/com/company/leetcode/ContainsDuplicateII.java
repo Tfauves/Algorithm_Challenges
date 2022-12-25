5ce34d100c6dc7f70ee36909c40fcfb1447d0eb4
Travis J <tfauvelle@gmail.com>
2022-12-25 07:07:16 -0500

    completed

--- src/com/company/leetcode/ContainsDuplicateII.java
+++ src/com/company/leetcode/ContainsDuplicateII.java
@@ -3,7 +3,6 @@ package com.company.leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
-// TODO: 12/22/2022 passing most cases but fails time complexity.
 public class ContainsDuplicateII {
     public static void main(String[] args) {
         int [] test = {1,2,3,1};
@@ -11,18 +10,34 @@ public class ContainsDuplicateII {
 
     }
 
-    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+   public static boolean containsNearbyDuplicate(int[] nums, int k) {
         boolean isDup = false;
-        for (int i = 0; i < nums.length -1; i ++) {
-            for (int j = i +1; j < nums.length; j++) {
-                if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
-                    isDup = true;
-
-                }
+        Map<Integer, Integer> indices = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer lastIndex = indices.put(nums[i], i);
+            if (lastIndex != null && (i - lastIndex) <= k) {
+                isDup = true;
             }
+
         }
-
+       System.out.println(isDup);
         return isDup;
-
     }
+
 }
+
+//// first attempt passed most test cases but failed time complexity////
+//    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+//        boolean isDup = false;
+//        for (int i = 0; i < nums.length -1; i ++) {
+//            for (int j = i +1; j < nums.length; j++) {
+//                if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+//                    isDup = true;
+//
+//                }
+//            }
+//        }
+//
+//        return isDup;
+//
+//    }
