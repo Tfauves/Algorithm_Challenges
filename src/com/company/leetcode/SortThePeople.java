25c590121e0237660a44003c2335193986c90594
Travis J <tfauvelle@gmail.com>
2023-01-03 15:57:17 -0500

    attempted

---
+++ src/com/company/leetcode/SortThePeople.java
@@ -0,0 +1,36 @@
+package com.company.leetcode;
+
+import java.util.*;
+
+public class SortThePeople {
+    public static void main(String[] args) {
+
+        String[] testNames = {"Mary","John","Emma"};
+        int[] testHeigts = {180,165,170};
+
+        sortPeople(testNames, testHeigts);
+    }
+
+    public static String[] sortPeople(String[] names, int[] heights) {
+        String[] queriedNames = new String[heights.length];
+        Map<Integer, String> heightMap = new HashMap<>();
+        List<String> sorted = new ArrayList<>();
+
+        for (int i = 0; i < heights.length; i++) {
+            heightMap.put(heights[i], names[i]);
+        }
+
+        for (Map.Entry<Integer, String> item : heightMap.entrySet()) {
+            Integer height = item.getKey();
+            String name = item.getValue();
+
+
+        }
+
+        System.out.println(sorted);
+        System.out.println(heightMap);
+        System.out.println(Arrays.toString(queriedNames));
+        return queriedNames;
+    }
+}
+
