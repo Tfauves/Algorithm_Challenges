00db3d85ba8d5f57a0f3dabfdbd7321a6c02bfdc
Travis J <tfauvelle@gmail.com>
2023-01-05 10:28:06 -0500

    completed

--- src/com/company/leetcode/SortThePeople.java
+++ src/com/company/leetcode/SortThePeople.java
@@ -6,29 +6,30 @@ public class SortThePeople {
     public static void main(String[] args) {
 
         String[] testNames = {"Mary","John","Emma"};
-        int[] testHeigts = {180,165,170};
+        int[] testHeights = {180,165,170};
 
-        sortPeople(testNames, testHeigts);
+        sortPeople(testNames, testHeights);
     }
 
     public static String[] sortPeople(String[] names, int[] heights) {
         String[] queriedNames = new String[heights.length];
-        Map<Integer, String> heightMap = new HashMap<>();
-        List<String> sorted = new ArrayList<>();
 
+        Map<Integer, String> heightMap = new HashMap<>();
         for (int i = 0; i < heights.length; i++) {
             heightMap.put(heights[i], names[i]);
         }
 
-        for (Map.Entry<Integer, String> item : heightMap.entrySet()) {
-            Integer height = item.getKey();
-            String name = item.getValue();
+        List<Integer> sorted = new ArrayList<>(heightMap.keySet());
+        sorted.sort(Collections.reverseOrder());
 
+        int queriedNamesArrIndex = 0;
+        for (int i : sorted) {
+            queriedNames[queriedNamesArrIndex] = heightMap.get(i);
+            queriedNamesArrIndex++;
         }
 
-        System.out.println(sorted);
-        System.out.println(heightMap);
-        System.out.println(Arrays.toString(queriedNames));
+//        System.out.println(heightMap);
+//        System.out.println(Arrays.toString(queriedNames));
         return queriedNames;
     }
 }
