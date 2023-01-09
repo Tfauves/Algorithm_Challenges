09de5ec9a58ff140c510335ddf737d1193cf15aa
Travis J <tfauvelle@gmail.com>
2023-01-09 09:55:01 -0500

    attempt

--- src/com/company/leetcode/ValidParentheses.java
+++ src/com/company/leetcode/ValidParentheses.java
@@ -13,28 +13,81 @@ public class ValidParentheses {
 
     public static boolean isValid(String s) {
         Stack<Character> container = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
-                container.push(currentChar);
-            } else {
-                if (container.isEmpty()) {
-                    return false;
-                }
-                char containerPeek = container.peek();
-                if ((currentChar == ')' && containerPeek != '(') || (currentChar == ']' && containerPeek != '[') || (currentChar == '}' && containerPeek != '{')) {
-                    return false;
-                } else {
-                    container.pop();
-                }
+
+        // convert string into char array and access the characters using for each loop.
+        for (char input : s.toCharArray())
+        {
+            // check ch
+            switch (input)
+            {
+                // open bracket then push it in stack.
+                // close bracket then pop the item and compare.
+                case '(':
+                case '{':
+                case '[':
+                    container.push(input);
+                    break;
+                case ')':
+                    if(container.isEmpty() || container.pop() != '(')
+
+                    // if the stack is empty then it means string have no open bracket.
+                    // hence it is invalid.
+                    {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if(container.isEmpty() || container.pop() != '{')
+                    {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if(container.isEmpty() || container.pop() != '[')
+                    {
+                        return false;
+                    }
+                    break;
             }
-
-
         }
-        return true;
+
+
+        // After the loop we have to check one more condition.
+        // return true only if the stack is empty.
+        // if stack is not empty that means we have unused brackets.
+
+        if(container.isEmpty())
+            return true;
+        else
+            return false;
+
     }
 
 }
+
+//public static boolean isValid(String s) {
+//    Stack<Character> container = new Stack<>();
+//    if (s.length() == 1) return false;
+//    for (int i = 0; i < s.length(); i++) {
+//        char currentChar = s.charAt(i);
+//        if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
+//            container.push(currentChar);
+//        } else {
+//            if (container.isEmpty()) {
+//                return false;
+//            }
+//            char containerPeek = container.peek();
+//            if ((currentChar == ')' && containerPeek != '(' )|| (currentChar == ']' &&      containerPeek != '[') || (currentChar == '}' && containerPeek != '{')) {
+//                return false;
+//            } else {
+//                container.pop();
+//            }
+//        }
+//
+//    }
+//    return true;
+//
+//}
 
 ///// first attempt 34 of 92 /////
 //public static boolean isValid(String s) {
