8fe0592f812bb5eff6557a0d593858ff55dc3d98
Travis J <tfauvelle@gmail.com>
2023-01-20 14:46:57 -0500

    attempt

--- src/com/company/leetcode/StudentAttendanceRecordI.java
+++ src/com/company/leetcode/StudentAttendanceRecordI.java
@@ -1,32 +1,47 @@
 package com.company.leetcode;
 
-// TODO: 1/19/2023 not passing because must take consecutive letters into consideration 
+// TODO: 1/20/2023 passing 106 out of 113 trouble edge case with array size of 0 
 public class StudentAttendanceRecordI {
     public static void main(String[] args) {
-        String test = "PPALLL";
+        String test = "AA";
 //        "PPALLP"
         checkRecord(test);
     }
     public static boolean checkRecord(String s) {
-        int pCount = 0;
-        int lCount = 0;
-        int aCount = 0;
-        int tooLate = 3;
-        int tooAbsent = 2;
+        // 'A count total' must be < 2
+        // L count total increments only if they are consecutive
+        // if L count total must be < 3
+        int totalACount = 0;
+        int totalLCount = 1;
+
         char[] inputChars = s.toCharArray();
 
-        for (Character ch : inputChars) {
-            switch (ch) {
-                case 'P' -> pCount++;
-                case 'L' -> lCount++;
-                case 'A' -> aCount++;
-                default -> {
+        for (int i = 0; i < inputChars.length -1; i++) {
+            int next = i + 1;
+            
+            if (inputChars[i] != 'P') {
+                if (inputChars[i] == 'A') {
+                    totalACount++;
+
+                    if (totalACount >= 2) {
+                        System.out.println(false);
+                        return false;
+                    }
+                }
+                if (inputChars[i] == 'L') {
+                    if (inputChars[next] == 'L') {
+                        totalLCount++;
+                    }
+
+                    if (totalLCount >= 3) {
+                        System.out.println(false);
+                        return false;
+                    }
                 }
             }
-
         }
-
-        return lCount < tooLate && aCount < tooAbsent;
+        System.out.println(true);
+        return true;
     }
 
 }
