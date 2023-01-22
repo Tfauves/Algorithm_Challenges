e69e88f00808a0d58d1166fd4b43d121ae8eb4d2
Travis J <tfauvelle@gmail.com>
2023-01-22 10:18:49 -0500

    completed

--- src/com/company/leetcode/RemoveDuplicatesFromSortedArray.java
+++ src/com/company/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -5,24 +5,25 @@ import java.util.Arrays;
 // TODO: 1/14/2023 stuck?????
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
-        int[] test = {1,1,2};
+        int[] test = {0,0,1,1,1,2,2,3,3,4};
 
         removeDuplicates(test);
 
     }
 
     public static int removeDuplicates(int[] nums) {
-        int moves = 0;
-        for (int i = 0; i < nums.length -1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i + 1] = nums[i + 2];
-                moves ++;
-                nums[i + 2] = -1;
+        int i = 1;
+        int len = nums.length;
+        while (i < len) {
+            if (nums[i] == nums[i - 1]) {
+                nums[i] = nums[len - 1];
+                // reduce array size by one
+                len--;
+            } else {
+                i++;
             }
         }
-
         System.out.println(Arrays.toString(nums));
-        System.out.println(nums.length -moves);
-        return nums.length -moves;
+        return len;
     }
 }
---
+++ src/com/company/leetcode/SearchInsertPosition.java
@@ -0,0 +1,28 @@
+package com.company.leetcode;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] test = {1,3,5,6};
+        int target = 7;
+
+        searchInsert(test, target);
+
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {return mid;}
+            else if (target < nums[mid]) { high = mid - 1;}
+            else {low = mid + 1;}
+        }
+        return low;
+    }
+}
