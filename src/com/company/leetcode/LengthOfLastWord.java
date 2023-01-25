6ead88a07a43c4da8e8cada7aea7ef18e19991a8
Travis J <tfauvelle@gmail.com>
2023-01-25 17:15:23 -0500

    completed

---
+++ src/com/company/leetcode/LengthOfLastWord.java
@@ -0,0 +1,20 @@
+package com.company.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+
+        String input = "   fly me   to   the moon  ";
+
+        lengthOfLastWord(input);
+    }
+
+    public static int lengthOfLastWord(String s) {
+        String[] strArr = s.split(" ");
+
+//        System.out.println(strArr[strArr.length -1].length());
+        return strArr[strArr.length -1].length();
+    }
+}
