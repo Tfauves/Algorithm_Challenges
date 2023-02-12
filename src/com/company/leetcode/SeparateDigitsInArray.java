7e15062d5d7333a4b8594f484ef43b59ad5146ed
Travis J <tfauvelle@gmail.com>
2023-02-12 11:06:15 -0500

    completed

---
+++ src/com/company/leetcode/SeparateDigitsInArray.java
@@ -0,0 +1,36 @@
+package com.company.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SeparateDigitsInArray {
+    public static void main(String[] args) {
+        int[] test = {13,25,83,77};
+
+        separateDigits(test);
+
+    }
+
+    public static int[] separateDigits(int[] nums) {
+        List<Integer> digi = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            String strNum = String.valueOf(nums[i]);
+
+            int len = strNum.length();
+
+            if (len > 0) {
+                for (int j = 0; j < len; j++) {
+                    digi.add(Integer.parseInt(String.valueOf(strNum.charAt(j))));
+
+                }
+            }
+
+        }
+        return digi.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
