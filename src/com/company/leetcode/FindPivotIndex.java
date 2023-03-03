99524ed841ddb66125611564190ba7824706b641
Travis J <tfauvelle@gmail.com>
2023-03-03 09:34:59 -0500

    completed

--- src/com/company/leetcode/FindPivotIndex.java
+++ src/com/company/leetcode/FindPivotIndex.java
@@ -1,6 +1,4 @@
 package com.company.leetcode;
-// TODO: 12/18/2022 fails a time complexity case 
-import java.util.Arrays;
 
 public class FindPivotIndex {
     public static void main(String[] args) {
@@ -11,37 +9,54 @@ public class FindPivotIndex {
     }
 
     public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
 
-        if (nums.length == 0) {
-            System.out.println(-1);
-            return -1;
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
         }
 
-        int last = nums.length -1;
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i -1];
-
-            System.out.println(Arrays.toString(nums));
-            last = nums.length -1;
-//            System.out.println(last);
-
-            
-        }
-
-        if (nums[last] - nums[0] == 0) {
-            System.out.println(0);
-            return 0;
-        }
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[last] - nums[i] == nums[i -1] ) {
-                System.out.println(i);
-
-                return i;
-            }
-        }
-
-        System.out.println("end" + -1);
+       for (int i = 0; i < nums.length; leftSum += nums[i++]) {
+           if (leftSum * 2 == totalSum - nums[i]) return i;
+       }
         return -1;
     }
 }
+///// first attempt failed time complexity
+//    public int pivotIndex(int[] nums) {
+//
+//        if (nums.length == 0) {
+//            System.out.println(-1);
+//            return -1;
+//        }
+//
+//        int last = nums.length -1;
+//        for (int i = 1; i < nums.length; i++) {
+//            nums[i] += nums[i -1];
+//
+//            System.out.println(Arrays.toString(nums));
+//            last = nums.length -1;
+////            System.out.println(last);
+//            // if (nums[last] - nums[0] == 0) {
+//
+//            //     System.out.println(0);
+//            //     return 0;
+//            // }
+//        }
+//
+//        if (nums[last] - nums[0] == 0) {
+//
+//            System.out.println(0);
+//            return 0;
+//        }
+//        for (int i = 1; i < nums.length; i++) {
+//            if (nums[last] - nums[i] == nums[i -1] ) {
+////                System.out.println(i);
+//
+//                return i;
+//            }
+//        }
+//
+////        System.out.println("end" + -1);
+//        return -1;
+//    }
