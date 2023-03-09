e6a41edd4bf2addc754724c9a4a08805d82d8bd6
Travis J <tfauvelle@gmail.com>
2023-03-09 13:07:14 -0500

    found issue and completed

--- src/com/company/leetcode/FinalPricesWithDiscount.java
+++ src/com/company/leetcode/FinalPricesWithDiscount.java
@@ -2,7 +2,6 @@ package com.company.leetcode;
 
 import java.util.Arrays;
 
-// TODO: 3/9/2023 not passing all  
 public class FinalPricesWithDiscount {
     public static void main(String[] args) {
         int[] input = {8,7,4,2,8,1,7,7,10,1};
@@ -18,7 +17,7 @@ public class FinalPricesWithDiscount {
 
         for (int i = 0; i < len; i++) {
             int discountPrice = prices[i];
-          for (int j = i +1; j < len -1; j++) {
+          for (int j = i +1; j < len; j++) {
               if (prices[j] <= prices[i]) {
                   discountPrice = prices[i] - prices[j];
                   break;
