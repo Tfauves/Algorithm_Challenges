2158145bb337c769a25362c2d5aa2077945bf6a2
Travis J <tfauvelle@gmail.com>
2023-03-30 10:00:23 -0400

    completed

--- src/com/company/leetcode/PlusOne.java
+++ src/com/company/leetcode/PlusOne.java
@@ -1,17 +1,9 @@
 package com.company.leetcode;
 
-import com.company.hackerRank.javaprep.BigInt;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-// TODO: 1/30/2023 long is not large enough for certain test cases needs bigInteger but still cannot get it workiing. 
 public class PlusOne {
     public static void main(String[] args) {
 
-        int [] test = {1,2,3};
+        int [] test = {9};
 //9,8,7,6,5,4,3,2,1,0
         plusOne(test);
     }
