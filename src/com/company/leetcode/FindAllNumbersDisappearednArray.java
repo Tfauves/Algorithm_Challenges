4f4d03d2842cc52a9b11e04ce7090c7a9b109e49
Travis J <tfauvelle@gmail.com>
2023-04-02 08:29:12 -0400

    completed

--- src/com/company/leetcode/FindAllNumbersDisappearednArray.java
+++ src/com/company/leetcode/FindAllNumbersDisappearednArray.java
@@ -1,36 +1,26 @@
 package com.company.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-// TODO: 4/1/2023 need to handle duplicate values??? 
 public class FindAllNumbersDisappearednArray {
     public static void main(String[] args) {
 
-        int[] input = {1,1};
+        int[] input = {4,3,2,7,8,2,3,1};
         findDisappearedNumbers(input);
     }
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> misVals = new ArrayList<>();
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length -1; i++) {
-            int diff = nums[i + 1] - nums[i];
-            if (nums[i] < nums[i + 1]) {
-                if (diff == 0) continue;
-                if (diff > 1) {
-                    for (int j = nums[i] + 1; j < nums[i + 1]; j++) {
-                        misVals.add(j);
-                    }
-                }
-
-
-            }
-
+        Set<Integer> numSet = new HashSet<>();
+        for (int item : nums) {
+            numSet.add(item);
         }
-
-        System.out.println(misVals);
-        return misVals;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= nums.length; i++) {
+            if (!numSet.contains(i)) {
+                list.add(i);
+            }
+        }
+        System.out.println(list);
+        return list;
     }
 }
