926a609fe68a519ef15b120064a049d4b87f4348
Travis J <tfauvelle@gmail.com>
2023-05-10 09:56:30 -0400

    completed

--- src/com/company/leetcode/IceCreamBars.java
+++ src/com/company/leetcode/IceCreamBars.java
@@ -1,6 +1,7 @@
 package com.company.leetcode;
 
-// TODO: 1/7/2023 not passing all cases 
+import java.util.Arrays;
+
 public class IceCreamBars {
     public static void main(String[] args) {
         int [] test = {7,3,3,6,6,6,10,5,9,2};
@@ -8,20 +9,40 @@ public class IceCreamBars {
         maxIceCream(test, 56);
     }
     public static int maxIceCream(int[] costs, int coins) {
-        int maxPrice = 0;
-        int maxBar = 0;
+        int barCount = 0;
+        int rollingTotal = 0;
 
-        for (int cost : costs) {
-            if (cost > coins) return 0;
-            if (maxPrice == coins) break;
-            maxPrice += cost;
+        Arrays.sort(costs);
+
+        for (Integer cost : costs) {
+            if (cost + rollingTotal <= coins) {
+                barCount++;
+                rollingTotal += cost;
+            }
         }
-        maxBar = maxPrice - coins;
-        if (maxPrice >= coins) return maxBar;
 
-        if (maxPrice <= coins) return costs.length;
-
-        return maxBar;
-
+        System.out.println(barCount);
+        return barCount;
     }
+
+    /// first attempt
+//    public static int maxIceCream(int[] costs, int coins) {
+//        int maxPrice = 0;
+//        int maxBar = 0;
+//        Arrays.sort(costs);
+//
+//        for (int cost : costs) {
+//            if (cost > coins) return 0;
+//            if (maxPrice == coins) break;
+//            maxPrice += cost;
+//        }
+//        maxBar = maxPrice - coins;
+//        if (maxPrice >= coins) return maxBar;
+//
+//        if (maxPrice <= coins) return costs.length;
+//
+//        System.out.println(maxBar);
+//        return maxBar;
+//
+//    }
 }
