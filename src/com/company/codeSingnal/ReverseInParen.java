6738003c28ad06f572a327e8b6d90f90be731705
Travis J <tfauvelle@gmail.com>
2023-05-13 08:25:34 -0400

    completed

--- src/com/company/codeSingnal/ReverseInParen.java
+++ src/com/company/codeSingnal/ReverseInParen.java
@@ -1,8 +1,7 @@
 package com.company.codeSingnal;
 
-import java.util.Stack;
 
-// TODO: 4/17/2023 not passing nested parens
+
 public class ReverseInParen {
     public static void main(String[] args) {
         String input = "foo(bar)baz";
@@ -12,36 +11,57 @@ public class ReverseInParen {
     }
 
     public static String solution(String inputString) {
-        StringBuilder output = new StringBuilder();
-        Stack<Character> charStack = new Stack<>();
-        char[] charInput = inputString.toCharArray();
-        int i = 0;
+        return reverseInParentheses(inputString);
+    }
 
+    public static String reverseInParentheses(String inputString) {
 
-        for (; i < charInput.length; i++) {
-            if (charInput[i] == '(') {
-                for (int j = i +1; charInput[j] != ')'; j++) {
-                    charStack.push(charInput[j]);
-                    i = j + 1;
-                }
-
-            }
-            while (!charStack.isEmpty()) {
-                output.append(charStack.pop());
-
-            }
-
-            if (charInput[i] != '(' && charInput[i] != ')') {
-                output.append(charInput[i]);
-
-            }
-
+        StringBuilder str = new StringBuilder();
+        int start = 0;
+        int end = inputString.length() - 1;
+        str.setLength(0);
+        if (inputString.contains("(")) {
+            start = inputString.lastIndexOf("(");
+            end = inputString.indexOf(")", start);
+            str.append(inputString, start+1, end);
+            return reverseInParentheses(inputString.substring(0, start) + str.reverse().toString() + inputString.substring(end+1));
         }
-
-
-        System.out.println(charStack);
-        System.out.println(output.toString());
-        return output.toString();
+        System.out.println(inputString);
+        return inputString;
     }
 
 }
+
+///first attempt
+//    public static String solution(String inputString) {
+//        StringBuilder output = new StringBuilder();
+//        Stack<Character> charStack = new Stack<>();
+//        char[] charInput = inputString.toCharArray();
+//        int i = 0;
+//
+//
+//        for (; i < charInput.length; i++) {
+//            if (charInput[i] == '(') {
+//                for (int j = i +1; charInput[j] != ')'; j++) {
+//                    charStack.push(charInput[j]);
+//                    i = j + 1;
+//                }
+//
+//            }
+//            while (!charStack.isEmpty()) {
+//                output.append(charStack.pop());
+//
+//            }
+//
+//            if (charInput[i] != '(' && charInput[i] != ')') {
+//                output.append(charInput[i]);
+//
+//            }
+//
+//        }
+//
+//
+//        System.out.println(charStack);
+//        System.out.println(output.toString());
+//        return output.toString();
+//    }
