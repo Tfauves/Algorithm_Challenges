b143b22b7d7f6b835467afdb88f7685eda360dad
Travis J <tfauvelle@gmail.com>
2023-05-17 15:27:49 -0400

    completed

--- src/com/company/leetcode/BuyAndSellStock.java
+++ src/com/company/leetcode/BuyAndSellStock.java
@@ -9,18 +9,39 @@ public class BuyAndSellStock {
     }
 
     public static int maxProfit(int[] prices) {
-        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
 
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                int potentialProfit = prices[j] - prices[i];
-                if (potentialProfit > 0 && potentialProfit > maxProfit) {
-                    maxProfit = potentialProfit;
-                }
+        for (int i = 0; i < prices.length; i++){
+
+            if (prices[i] < min) {
+                min = prices[i];
             }
+
+            if (prices[i] - min > profit) {
+
+                profit = prices[i]-min;
+            }
+
         }
 
-        System.out.println(maxProfit);
-        return maxProfit;
+        return profit;
     }
+
+
+
+    // first attempt failing time complexity
+//    int maxProfit = 0;
+//
+//        for (int i = 0; i < prices.length; i++) {
+//        for (int j = i + 1; j < prices.length; j++) {
+//            int potentialProfit = prices[j] - prices[i];
+//            if (potentialProfit > 0 && potentialProfit > maxProfit) {
+//                maxProfit = potentialProfit;
+//            }
+//        }
+//    }
+//
+//        System.out.println(maxProfit);
+//        return maxProfit;
 }
