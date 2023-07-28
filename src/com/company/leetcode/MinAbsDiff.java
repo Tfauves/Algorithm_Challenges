325510384c8840e8d54bb8a34c094de63187d2fe
Travis J <tfauvelle@gmail.com>
2023-07-28 16:04:19 -0400

    completed

--- src/com/company/leetcode/MinAbsDiff.java
+++ src/com/company/leetcode/MinAbsDiff.java
@@ -4,33 +4,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-// TODO: 7/10/2023 needs work 
+
 public class MinAbsDiff {
     public static void main(String[] args) {
-        int[] arr = {4, 2, 1, 3, 5};
-        List<int[]> pairs = minimumAbsDifferencePairs(arr);
-        for (int[] pair : pairs) {
-            System.out.println(Arrays.toString(pair));
-        }
+        int[] arr = {4,2,1,3};
+       minimumAbsDifference(arr);
     }
 
-    public static List<int[]> minimumAbsDifferencePairs(int[] arr) {
+    public static List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
+
         int minDiff = Integer.MAX_VALUE;
+        List<List<Integer>> result = new ArrayList<>();
+
         for (int i = 1; i < arr.length; i++) {
-            int diff = Math.abs(arr[i] - arr[i - 1]);
+            int diff = arr[i] - arr[i - 1];
+
             if (diff < minDiff) {
+
                 minDiff = diff;
+                result.clear();
+            }
+
+            if (diff == minDiff) {
+
+                List<Integer> pair = new ArrayList<>();
+                pair.add(arr[i - 1]);
+                pair.add(arr[i]);
+                result.add(pair);
             }
         }
 
-        List<int[]> result = new ArrayList<>();
-        for (int i = 1; i < arr.length; i++) {
-            if (Math.abs(arr[i] - arr[i - 1]) == minDiff) {
-                result.add(new int[]{arr[i - 1], arr[i]});
-            }
-        }
-
+        System.out.println(result);
         return result;
     }
 }
