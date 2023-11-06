ac435b86b32979cfcf61cac10a6cb5fd29cffc21
Travis J <tfauvelle@gmail.com>
2023-11-06 15:53:43 -0500

    attempte:

--- src/com/company/leetcode/FindWinnerOfArrayGame.java
+++ src/com/company/leetcode/FindWinnerOfArrayGame.java
@@ -1,7 +1,6 @@
 package com.company.leetcode;
 
-import java.util.Arrays;
-
+// TODO: 11/6/2023 fails time limit 
 public class FindWinnerOfArrayGame {
     public static void main(String[] args) {
         int[] input = {2,1,3,5,4,6,7};
@@ -11,16 +10,31 @@ public class FindWinnerOfArrayGame {
     }
 
     public static int getWinner(int[] arr, int k) {
+        int winner = arr[0];
+        int roundWins = 0;
 
-      int[] shiftedArray = new int[arr.length];
+        for (int i = 1; i < arr.length; i++) {
 
-      for (int i = 1; i < arr.length; i++) {
-          shiftedArray[i -1] = arr[i];
-      }
-      shiftedArray[shiftedArray.length -1] = arr[0];
+            if (arr[i] > winner) {
+                winner = arr[i];
+                roundWins = 1;
+            } else {
+                roundWins++;
+            }
 
-        System.out.println(Arrays.toString(shiftedArray));
-        return shiftedArray[0];
+            if (roundWins == k) return winner;
+
+            int temp = arr[i];
+
+            for (int j = i; j > 1; j--) {
+                arr[j] = arr[j -1];
+            }
+
+            arr[1] = temp;
+
+        }
+
+        return winner;
     }
 
 }
